ad4214ef1f42da1a089dddfb5d4fa3d32170e3e5
makotthi <gurifonnkatou@gmail.com>
2021-05-03 17:19:29 +0900

    MyPickerViewクラスを作成

---
+++ MahjongCalculator/MahjongCalculator/Views/MyPickerView/MyPickerView.swift
@@ -0,0 +1,12 @@
+//
+//  MyPickerView.swift
+//  MahjongCalculator
+//
+//  Created by 谷藤真 on 2021/05/03.
+//
+
+import UIKit
+
+class MyPickerView: UIPickerView {
+    var titleList: [String] = []
+}
