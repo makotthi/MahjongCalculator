82b55f58c6ca928767a0e6afad5f596f429c4379
makotthi <gurifonnkatou@gmail.com>
2021-05-03 16:37:58 +0900

    UIパーツを配置

--- MahjongCalculator/MahjongCalculator/Controllers/InputViewController/InputViewController.swift
+++ MahjongCalculator/MahjongCalculator/Controllers/InputViewController/InputViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class InputViewController: UIViewController {
+    
+    @IBOutlet weak var humansTextField: UITextField!
+    @IBOutlet weak var winnerTextField: UITextField!
+    @IBOutlet weak var yakuTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+// MARK: Action
+extension InputViewController {
+    @IBAction func calculateButtonTapped(_ sender: Any) {
+    }
+}
